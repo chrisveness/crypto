bc59b37c7724c9bfca6b5728f07eee506cd9f5fb
Chris Veness <chrisv@movable-type.co.uk>
2016-10-18 11:33:38 +0100

    Lowercase hex constants

--- tea-block.js
+++ tea-block.js
@@ -88,7 +88,7 @@ Tea.encode = function(v, k) {
     if (v.length < 2) v[1] = 0;  // algorithm doesn't work for n<2 so fudge by adding a null
     var n = v.length;
 
-    var z = v[n-1], y = v[0], delta = 0x9E3779B9;
+    var z = v[n-1], y = v[0], delta = 0x9e3779b9;
     var mx, e, q = Math.floor(6 + 52/n), sum = 0;
 
     while (q-- > 0) {  // 6 + 52/n operations gives between 6 & 32 mixes on each word
@@ -115,7 +115,7 @@ Tea.encode = function(v, k) {
 Tea.decode = function(v, k) {
     var n = v.length;
 
-    var z = v[n-1], y = v[0], delta = 0x9E3779B9;
+    var z = v[n-1], y = v[0], delta = 0x9e3779b9;
     var mx, e, q = Math.floor(6 + 52/n), sum = q*delta;
 
     while (sum != 0) {
@@ -155,7 +155,7 @@ Tea.strToLongs = function(s) {
 Tea.longsToStr = function(l) {
     var str = '';
     for (var i=0; i<l.length; i++) {
-        str += String.fromCharCode(l[i] & 0xFF, l[i]>>>8 & 0xFF, l[i]>>>16 & 0xFF, l[i]>>>24 & 0xFF);
+        str += String.fromCharCode(l[i] & 0xff, l[i]>>>8 & 0xff, l[i]>>>16 & 0xff, l[i]>>>24 & 0xff);
     }
     return str;
 };
